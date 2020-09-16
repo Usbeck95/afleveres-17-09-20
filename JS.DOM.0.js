378b7c0bc1f8e89d35a0644eb03638651f3f8113
Carina <carinausbeck@live.dk>
2020-09-16 20:08:16 +0200

    first commit

---
+++ JS.DOM.0.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const play = function() {
+    return Math.floor(Math.random() * 6 + 1);
+}
---
+++ JS.DOM.0execute.js
@@ -0,0 +1,502 @@
+// first suggestion without default at end works fine
+// second suggestion with default at end works
+// third works fine too
+// I don't see any differences in the three suggestions
+
+
+'use strict';
+
+let ones = 0;
+let twos = 0;
+let threes = 0;
+let fours = 0;
+let fives = 0;
+let sixes = 0;
+
+let r = play();
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+default:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+case 6:
+    sixes++;
+}
+
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+    
+switch (play()) {
+    case 1:
+        (r === 1);
+        ones++;
+    break;
+    default:
+        (r === 2);
+        twos++;
+    break;
+    case 3:
+        (r === 3)
+        threes++;
+    break;
+    case 4:
+        (r === 4)
+        fours++;
+    break;
+    case 5:
+        (r === 5)
+        fives++;
+    break;
+    case 6:
+        sixes++;
+    }
+
+/*
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+switch (play()) {
+case 1:
+    (r === 1);
+    ones++;
+break;
+case 2:
+    (r === 2);
+    twos++;
+break;
+case 3:
+    (r === 3)
+    threes++;
+break;
+case 4:
+    (r === 4)
+    fours++;
+break;
+case 5:
+    (r === 5)
+    fives++;
+break;
+default:
+    sixes++;
+}
+
+*/
+
+document.write(`<p>`);
+document.write(`1's: ${ones}<br/>`);
+document.write(`2's: ${twos}<br/>`);
+document.write(`3's: ${threes}<br/>`);
+document.write(`4's: ${fours}<br/>`);
+document.write(`5's: ${fives}<br/>`);
+document.write(`6's: ${sixes}<br/>`);
+document.write(`total: ${ones+twos+threes+fours+fives+sixes}</p>`);
